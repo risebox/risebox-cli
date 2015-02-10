173540741e31259096233d16ff86746fb66c8dd1
nna <nico.nardone@gmail.com>
2015-02-10 15:39:32 +0100

    cascaded module syntax

--- lib/rb/client/config.rb
+++ lib/rb/client/config.rb
@@ -1,18 +1,20 @@
-module RB::Client
-  class << self
-    attr_accessor :configuration
-  end
+module RB
+  module Client
+    class << self
+      attr_accessor :configuration
+    end
 
-  def self.configure
-    self.configuration ||= Configuration.new
-    yield(configuration) if block_given?
-  end
+    def self.configure
+      self.configuration ||= Configuration.new
+      yield(configuration) if block_given?
+    end
 
-  class Configuration
-    attr_accessor :api_url
+    class Configuration
+      attr_accessor :api_url
 
-    def initialize
-      @api_url = 'https://risebox-api.herokuapp.com'
+      def initialize
+        @api_url = 'https://risebox-api.herokuapp.com'
+      end
     end
   end
 end
--- lib/rb/client/connection.rb
+++ lib/rb/client/connection.rb
@@ -1,68 +1,71 @@
 require 'faraday'
 
-module RB::Client::Connection
+module RB
+  module Client
+    module Connection
 
-  def get_connection args
-    Faraday.new(args)
+      def get_connection args
+        Faraday.new(args)
+      end
+
+      def perform_request verb, conn, url, form_params=nil
+        begin
+          result = http_call(verb, conn, url, form_params)
+        rescue Faraday::Error::TimeoutError
+          return [false, message: 'Timeout']
+        end
+        case result.status
+        when 200
+          [true, sym_keys(JSON.parse(result.body))]
+        when 403
+          [false, sym_keys(JSON.parse(result.body))]
+        when 404
+          [false, sym_keys(JSON.parse(result.body))]
+        when 500
+          [false, sym_keys(JSON.parse(result.body))]
+        end
+      end
+
+      def perform_request! verb, conn, url, form_params=nil
+        begin
+          result = http_call(verb, conn, url, form_params)
+        rescue Faraday::Error::TimeoutError
+          raise RB::Client::TimeoutError, 'RB Client Timeout'
+        end
+        case result.status
+        when 200
+          sym_keys(JSON.parse(result.body))
+        when 403
+          raise RB::Client::ForbiddenError, 'RB Client Bad credentials'
+        when 404
+          raise RB::Client::NotFoundError, 'RB Client Ressource not found'
+        when 500
+          raise RB::Client::AppError, 'RB Client Application error'
+        end
+      end
+
+    private
+      def http_call verb, conn, url, form_params=nil
+        result = conn.public_send(verb) do |req|
+          req.url url
+          req.body = form_params if verb == :post
+          req.options[:timeout]       = 5
+          req.options[:open_timeout]  = 2
+        end
+      end
+
+      def sym_keys hash
+        transfo_keys hash do |key|
+          key.to_sym rescue key
+        end
+      end
+
+      def transfo_keys hash, &block
+        hash.keys.each do |key|
+          hash[yield(key)] = hash.delete(key)
+        end
+        hash
+      end
+    end
   end
-
-  def perform_request verb, conn, url, form_params=nil
-    begin
-      result = http_call(verb, conn, url, form_params)
-    rescue Faraday::Error::TimeoutError
-      return [false, message: 'Timeout']
-    end
-    case result.status
-    when 200
-      [true, sym_keys(JSON.parse(result.body))]
-    when 403
-      [false, sym_keys(JSON.parse(result.body))]
-    when 404
-      [false, sym_keys(JSON.parse(result.body))]
-    when 500
-      [false, sym_keys(JSON.parse(result.body))]
-    end
-  end
-
-  def perform_request! verb, conn, url, form_params=nil
-    begin
-      result = http_call(verb, conn, url, form_params)
-    rescue Faraday::Error::TimeoutError
-      raise RB::Client::TimeoutError, 'RB Client Timeout'
-    end
-    case result.status
-    when 200
-      sym_keys(JSON.parse(result.body))
-    when 403
-      raise RB::Client::ForbiddenError, 'RB Client Bad credentials'
-    when 404
-      raise RB::Client::NotFoundError, 'RB Client Ressource not found'
-    when 500
-      raise RB::Client::AppError, 'RB Client Application error'
-    end
-  end
-
-private
-  def http_call verb, conn, url, form_params=nil
-    result = conn.public_send(verb) do |req|
-      req.url url
-      req.body = form_params if verb == :post
-      req.options[:timeout]       = 5
-      req.options[:open_timeout]  = 2
-    end
-  end
-
-  def sym_keys hash
-    transfo_keys hash do |key|
-      key.to_sym rescue key
-    end
-  end
-
-  def transfo_keys hash, &block
-    hash.keys.each do |key|
-      hash[yield(key)] = hash.delete(key)
-    end
-    hash
-  end
-
 end
